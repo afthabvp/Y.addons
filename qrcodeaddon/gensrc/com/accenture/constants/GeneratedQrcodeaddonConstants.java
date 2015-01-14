3fa08382fceeab2507a95e7f774f60c28dc62597
swapniilpandey <swapnil.a.pandey@accenture.com>
2015-01-14 17:45:24 +0530

    fix for wishlist link and UCOID
    Updated qrcodeaddon

---
+++ qrcodeaddon/gensrc/com/accenture/constants/GeneratedQrcodeaddonConstants.java
@@ -0,0 +1,57 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jan 14, 2015 5:20:27 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.accenture.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedQrcodeaddonConstants
+{
+	public static final String EXTENSIONNAME = "qrcodeaddon";
+	public static class TC
+	{
+		public static final String COLLECTORDER = "CollectOrder".intern();
+		public static final String CONFIG = "Config".intern();
+		public static final String CSRCUSTOMERDETAILS = "CSRCustomerDetails".intern();
+		public static final String CSRSTORESTATUS = "CSRStoreStatus".intern();
+		public static final String UCOIDONORDERCONFIRMATIONCOMPONENT = "UCOIDOnOrderConfirmationComponent".intern();
+	}
+	public static class Attributes
+	{
+		public static class Customer
+		{
+			public static final String UUID = "UUID".intern();
+		}
+		public static class Order
+		{
+			public static final String UCOID = "UCOID".intern();
+		}
+		public static class User
+		{
+			public static final String RECENTLYVIEWEDPRODUCTS = "recentlyviewedproducts".intern();
+		}
+	}
+	public static class Enumerations
+	{
+		public static class CSRStoreStatus
+		{
+			public static final String INSERVICE = "InService".intern();
+			public static final String LOGGEDIN = "LoggedIn".intern();
+			public static final String NOTHANKS = "NoThanks".intern();
+			public static final String COMPLETED = "Completed".intern();
+		}
+	}
+	
+	protected GeneratedQrcodeaddonConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
