c6fbb80fecbe6f7edd899ef4882be311cb356de6
swarnimagupta <swarnima.gupta@accenture.com>
2015-02-13 09:23:32 +0530

    New UI css, js and  images for customers dashboard

---
+++ beacon/Hybris web-app addon/installation/qrcodeaddon/acceleratoraddon/web/webroot/_ui/desktop/common/bnc_js/new/library.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	tabModule.init();	
+
+$("#slider1").tinycarousel();
+$( "#datepicker" ).datepicker({
+showOn: "button",
+buttonImage: "images/cal.png",
+buttonImageOnly: true,
+buttonText: "Select date"
+});
+
+$( "#datepicker1" ).datepicker({
+showOn: "button",
+buttonImage: "images/cal.png",
+buttonImageOnly: true,
+buttonText: "Select date"
+});
+
+$("#usrdd").hover(function() {
+$(".ddmenu").show();
+})
+
+$("#hdr").mouseleave(function() {
+$(".ddmenu").hide();
+})
+
+$("#lmenu").hide();
+$(".mClose").click(function() {
+$("#lmenu").animate({width: 'toggle'},200);
+});
+
+ $(".slmscr").slimScroll({railVisible: true, railColor: '#f00'});
+ $(".slmscr1").slimScroll({railVisible: true, railColor: '#f00'});
+$("#slimtest").slimScroll();
+
+
+});
