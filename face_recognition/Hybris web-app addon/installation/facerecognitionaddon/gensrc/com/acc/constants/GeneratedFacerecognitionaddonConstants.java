8f0f063be5b51da9518273928f0f7fc1f1a500ad
swarnimagupta <swarniima@gmail.com>
2015-10-05 11:17:34 +0530

    Added face recognition code

---
+++ face_recognition/Hybris web-app addon/installation/facerecognitionaddon/gensrc/com/acc/constants/GeneratedFacerecognitionaddonConstants.java
@@ -0,0 +1,62 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Aug 28, 2015 3:11:41 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.acc.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedFacerecognitionaddonConstants
+{
+	public static final String EXTENSIONNAME = "facerecognitionaddon";
+	public static class TC
+	{
+		public static final String COMPLEXION = "Complexion".intern();
+		public static final String FACERECOGNITIONCOMPONENT = "FaceRecognitionComponent".intern();
+		public static final String IMAGEQUALITY = "ImageQuality".intern();
+		public static final String PERSISTCUSTOMERIMAGESCRONJOB = "PersistCustomerImagesCronJob".intern();
+	}
+	public static class Attributes
+	{
+		public static class CSRCustomerDetails
+		{
+			public static final String AGE = "age".intern();
+			public static final String COMPLEXION = "complexion".intern();
+			public static final String GENDER = "gender".intern();
+			public static final String IMAGEURL = "imageUrl".intern();
+		}
+		public static class Customer
+		{
+			public static final String AGE = "age".intern();
+			public static final String COMPLEXION = "complexion".intern();
+			public static final String IMAGEQUALITY = "imageQuality".intern();
+		}
+	}
+	public static class Enumerations
+	{
+		public static class Complexion
+		{
+			public static final String BLACK = "Black".intern();
+			public static final String WHEATISH = "Wheatish".intern();
+			public static final String FAIR = "Fair".intern();
+			public static final String DARKBROWN = "DarkBrown".intern();
+			public static final String VERYFAIR = "VeryFair".intern();
+		}
+	}
+	public static class Relations
+	{
+		public static final String CUSTOMER2IMAGEQUALITYREL = "Customer2ImageQualityRel".intern();
+	}
+	
+	protected GeneratedFacerecognitionaddonConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ face_recognition/Hybris web-app addon/installation/qrcodeaddon/gensrc/com/accenture/constants/GeneratedQrcodeaddonConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Aug 28, 2015 3:11:41 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.accenture.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedQrcodeaddonConstants
+{
+	public static final String EXTENSIONNAME = "qrcodeaddon";
+	
+	protected GeneratedQrcodeaddonConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
